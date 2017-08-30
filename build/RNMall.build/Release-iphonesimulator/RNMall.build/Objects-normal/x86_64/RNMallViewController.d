ed9ed666db2d9d7af96ca35fff7f1f9e696e88dd
lixu <lixu_sx@qiyi.com>
2017-08-30 10:52:19 +0800

    auto create new branch

--- build/RNMall.build/Release-iphonesimulator/RNMall.build/Objects-normal/x86_64/RNMallViewController.d
+++ build/RNMall.build/Release-iphonesimulator/RNMall.build/Objects-normal/x86_64/RNMallViewController.d
@@ -3,11 +3,11 @@ dependencies: \
   /Users/iqiyi/Downloads/Develop/Developer/Projects/Product/MallDevelop/RNMall/RNMall/RNMallViewController.h \
   /Users/iqiyi/Downloads/Develop/Developer/Projects/Product/MallDevelop/RNMall/RNMall/RNMallVIPViewController.h \
   /Users/iqiyi/Downloads/Develop/Developer/Projects/Product/MallDevelop/RNMall/RNMall/RNMallOrdersViewController.h \
-  ../RNApp/Pods/Headers/Private/React/RCTRootView.h \
-  ../RNApp/Pods/Headers/Private/React/RCTBridge.h \
-  ../RNApp/Pods/Headers/Private/React/RCTBridgeDelegate.h \
-  ../RNApp/Pods/Headers/Private/React/RCTJavaScriptLoader.h \
-  ../RNApp/Pods/Headers/Private/React/RCTDefines.h \
-  ../RNApp/Pods/Headers/Private/React/RCTBridgeModule.h \
-  ../RNApp/Pods/Headers/Private/React/RCTFrameUpdate.h \
-  ../RNApp/Pods/Headers/Private/React/RCTInvalidating.h
+  ../RNApp/Pods/React_debug/ReactInclude/RCTRootView.h \
+  ../RNApp/Pods/React_debug/ReactInclude/RCTBridge.h \
+  ../RNApp/Pods/React_debug/ReactInclude/RCTBridgeDelegate.h \
+  ../RNApp/Pods/React_debug/ReactInclude/RCTJavaScriptLoader.h \
+  ../RNApp/Pods/React_debug/ReactInclude/RCTDefines.h \
+  ../RNApp/Pods/React_debug/ReactInclude/RCTBridgeModule.h \
+  ../RNApp/Pods/React_debug/ReactInclude/RCTFrameUpdate.h \
+  ../RNApp/Pods/React_debug/ReactInclude/RCTInvalidating.h
